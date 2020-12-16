beaf91bab85dc5e70513f7883143afc2c7a5a628
FOUGHTYEW <69669267+Failpfailp@users.noreply.github.com>
2020-12-17 00:23:57 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+//How Bot Online 24/7
+
+//the most Simple
+
+//Discord : 𝙁𝘾 么 Glitch Editz#5631
+//Github : FOUGHTYEW/Failpfailp
+
+const fetch = require('node-fetch')
+
+setInterval(async () => {
+  await fetch('Your glitch project live page URL here').then(console.log('Pinged!'))
+}, 240000) //to be no more than 240000
+
+//copy the live link
+
+//this works all discord.js v12/v11 / commands handlers
